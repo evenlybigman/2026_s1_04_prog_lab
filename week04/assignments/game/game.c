1d49d32aeb01418b929ba075a8c7825bd100657f
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-03-27 17:29:13 +0900

    feat: 어드벤처 게임 v1 초기 구현 fix: time.h 추가, 몬스터 스폰 범위 % 9 → % 10 수정, 몬스터2 재배치 버그 수정 style: print_map 함수 정의부 K&R 스타일 → ANSI 스타일 변경, 선언부 제거 fix: 몬스터 스폰 위치 조건 if → while로 변경 feat: 몬스터 이동 기능 추가, MAPSIZE 상수 정의 fix: 몬스터 이동 rand 오타 수정, 범위 체크 else 누락 수정 fix: 플레이어 이동 범위 MAPSIZE 상수로 수정 refactor: 미사용 변수 제거, 불필요한 return 제거

--- week04/assignments/game/game.c
+++ week04/assignments/game/game.c
@@ -1,18 +1,20 @@
-﻿//어드벤처 게임 v1.
+﻿//어드벤처 게임 v2.
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
-void print_map(int px,int py,int gx,int gy,int mx1, int my1,int mx2,int my2);
+#define MAPSIZE_X 10
+#define MAPSIZE_Y 10
 // 주인공 #, 몬스터 M, 금 G
-//system("cls");
+//system("cls"); 화면 삭제
 
-void print_map(px, py,gx,gy,mx1,my1,mx2,my2) {
+//게임맵 출력
+void print_map(int px, int py, int gx, int gy, int mx1, int my1, int mx2, int my2) {
 	system("cls");
-	char map[10][10];
-	for (int x = 0; x < 10; x++) {
-		for (int y = 0; y < 10; y++) {
+	char map[MAPSIZE_X][MAPSIZE_Y];
+	for (int x = 0; x < MAPSIZE_X; x++) {
+		for (int y = 0; y < MAPSIZE_Y; y++) {
 			map[x][y] = '.';
 		}
 	}
@@ -20,41 +22,74 @@ void print_map(px, py,gx,gy,mx1,my1,mx2,my2) {
 	map[mx2][my2] = 'M';
 	map[gx][gy] = 'G';
 	map[px][py] = '#';
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
+	for (int i = 0; i < MAPSIZE_X; i++) {
+		for (int j = 0; j < MAPSIZE_Y; j++) {
 			printf("%c ", map[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-int main() { //1,1 w -> 0, 1
+//몬스터 X좌표 이동 함수
+int monster_move_x(int px,int mx) {
+	if (px > mx) {
+		if (mx == MAPSIZE_X-1);
+		else mx++;
+		return mx;
+	}
+	else {
+		if (mx == 0);
+		else mx--;
+		return mx;
+	}
+}
+
+//몬스터 Y좌표 이동 함수
+int monster_move_y(int py,int my) {
+	if (py > my) {
+		if (my == MAPSIZE_Y-1);
+		else my++;
+		return my;
+	}
+	else {
+		if (my == 0);
+		else my--;
+		return my;
+	}
+}
+
+int main() {
 	srand(time(NULL));
 	// 각 유닛들 초기 위치 설정
+	char input;
 	int px = 0, py = 0;
-	int gx = 9, gy = 9;
+	int gx = MAPSIZE_X - 1, gy = MAPSIZE_Y - 1;
 	int mx1 = rand() % 10, my1 = rand() % 10;
 	int mx2 = rand() % 10, my2 = rand() % 10;
 	// 몬스터1 위치가 플레이어 위치거나 금의 위치면 위치 다시 설정)
-	if ((mx1 + my1) == 0 || (mx1 + my1) == 18) {
+	while ((mx1 == 0 && my1 == 0)||(mx1 == MAPSIZE_X - 1 && my1 == MAPSIZE_Y - 1)) {
 		mx1 = rand() % 10, my1 = rand() % 10;
 	}
 	// 몬스터2 위치가 플레이어 위치거나 금의 위치면 위치 다시 설정)
-	if ((mx2 + my2) == 0 || (mx2 + my2) == 18) {
+	while ((mx2 == 0 && my2 == 0) || (mx2 == MAPSIZE_X - 1 && my2 == MAPSIZE_Y - 1)) {
 		mx2 = rand() % 10, my2 = rand() % 10;
 	}
-	char input = '1';
+	//게임 방법 설명
 	printf("왼쪽 이동:a, 오른쪽 이동: d 위쪽 이동: w, 아래쪽 이동: s\n");
-	while (1) { // 0,9 -> d 0,9  1,0
+	//게임 진행 반복
+	while (1) {
+		//몬스터와 닿으면 패배
 		print_map(px, py,gx,gy,mx1,my1,mx2,my2);
 		if ((px == mx1 && py == my1) || (px == mx2 && py == my2)) {
 			printf("Lose");
 			break;
 		}
+		//금과 닿으면 승리
 		if (px == gx && py == gy) {
 			printf("WIN");
 			break;
 		}
+		//플레이어의 입력을 받고 이동
 		input = _getch();
 		switch (input) {
 		case 'w':
@@ -66,15 +101,24 @@ int main() { //1,1 w -> 0, 1
 			else py--;
 			break;
 		case 's':
-			if (px == 9);
+			if (px == MAPSIZE_X-1);
 			else px++;
 			break;
 		case 'd':
-			if (py == 9);
+			if (py == MAPSIZE_Y-1);
 			else py++;
 			break;
 		default:
 			continue;
 		}
+		//몬스터가 플레이어를 따라오게 이동
+		if (rand() % 2 == 0) {
+			mx1 = monster_move_x(px, mx1);
+		}
+		else my1 = monster_move_y(py, my1);
+		if (rand() % 2 == 0) {
+			mx2 = monster_move_x(px, mx2);
+		}
+		else my2 = monster_move_y(py,my2);
 	}
 }
