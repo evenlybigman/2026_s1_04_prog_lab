9c03e2182cedfe4ffd8bcfc4253a29364ce7ac20
Eve <evenlybigman@gmail.com>
2026-04-09 16:10:33 +0900

    한영사전 실습

---
+++ week06_sol/main.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define WORDS 5
+
+int main() {
+	int i, index;
+	char dic[WORDS][2][30] = {
+		{"book","책"},
+		{"boy","남자"},
+		{"computer","컴퓨터"},
+		{"language","언어"},
+		{"rain","비"},
+	};
+	char word[30];
+	while (1) {
+		printf("단어를 입력하시오. (종료: exit): ");
+		scanf("%s", &word);
+		if (strcmp(word, "exit") == 0) {
+			break;
+		}
+		for (int i = 0; i < WORDS; i++) {
+				if (strcmp(word, dic[i][0]) == 0) {
+					printf("%s : %s\n", dic [i][0], dic[i][1]);
+				}
+				else if (strcmp(word,dic[i][1]) == 0) {
+					printf("%s : %s\n", dic[i][0], dic[i][1]);
+				}
+				else printf("없는 단어입니다.\n");
+				break;
+			}
+		}
+	}
