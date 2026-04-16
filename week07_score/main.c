b6aad76dee301849b938e8ad1b064ccbf5126db2
Eve <evenlybigman@gmail.com>
2026-04-16 16:07:07 +0900

    성적 입력 프로그램

---
+++ week07_score/main.c
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef struct student {
+	int id;
+	char name[20];
+	double score;
+}Student;
+
+int main() {
+	FILE* fp = NULL;
+	fp = fopen("scores.txt", "w");
+	if (fp == NULL) {
+		fprintf(stderr,"socres.txt 파일을 찾을 수 없습니다.\n");
+	}
+
+	Student student[100];
+	char answer;
+	int count = 0;
+	int c;
+
+	while (1) {
+
+		printf("학번 : ");
+		scanf("%d", &student[count].id);
+		fputc(student[count].id,fp);
+		printf("이름 : ");
+		scanf("%s", student[count].name);
+		for (int i = 0; student[count].name != '\0'; i++) {
+			fputc(student[count].name[i], fp);
+		}
+		fputc(student[count].name, fp);
+		printf("성적 : ");
+		scanf("%d", &student[count].score);
+		
+		printf("데이터 추가를 계속? (y/n) : ");
+		scanf("%d", answer);
+		if (answer == 'y') {
+
+			continue;
+		}
+		else if (answer == 'n') {
+			printf("데이터 추가를 종료합니다");
+			break;
+		}
+		else {
+			printf("입력 오류");
+		}
+
+	}
+
+	return 0;
+}
---
+++ week07_score/main2.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	FILE* fp;
+	int number;
+	char name[20];
+	double score;
+	char ch;
+
+	fp = fopen("score.txt", "w");
+	if (fp == NULL) {
+		printf("성적 파일을 열 수 없습니다.\n");
+		exit(1);
+	}
+
+	do {
+		printf("학번: ");
+		scanf("%d", &number);
+
+		printf("이름: ");
+		scanf("%s", name);
+
+		printf("성적: ");
+		scanf("%lf", &score);
+
+		fprintf(fp, "%d %s %.2lf\n", number, name, score);
+		printf("데이터 추가를 계속? (y/n): ");
+		scanf(" %c", &ch);
+
+	} while (ch != 'n');
+
+	fclose(fp);
+	return 0;
+}
