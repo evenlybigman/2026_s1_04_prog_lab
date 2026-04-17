7fd44c286428e6ca36a39bb462215c8b8b6565e2
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-18 02:48:58 +0900

    로직 변경 배열에 텍스트 내용을 저장하는 방식 -> 문자 저장 후 바로 비교 (메모리 절약 가능, 길이 달라도 가능)

--- week07_textcmp/main.c
+++ week07_textcmp/main.c
@@ -4,56 +4,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
 	//포인터 선언
-	FILE* fp1, * fp2;
-	fp1 = fp2 = NULL;
-	char file1[1000], file2[1000];
-	char word1[100], word2[100];
+	FILE* fp1 = NULL, *fp2 = NULL;
+	char file_name1[100], file_name2[100];
+	int ch1, ch2;
+	int same = 1;
 	//file1 열기
 	printf("첫 번째 파일 이름: ");
-	scanf("%s", file1);
-	fp1 = fopen(file1, "r");
+	scanf("%s", file_name1);
+	fp1 = fopen(file_name1, "r");
 	if (fp1 == NULL) {
 		printf("첫 번째 파일을 찾을 수 없습니다.");
 		exit(1);
 	}
 	//file2 열기
 	printf("두 번째 파일 이름: ");
-	scanf("%s", file2);
-	fp2 = fopen(file2, "r");
+	scanf("%s", file_name2);
+	fp2 = fopen(file_name2, "r");
 	if (fp2 == NULL) {
 		printf("두 번째 파일을 찾을 수 없습니다.");
 		exit(1);
 	}
 	
-	//일치하면 "파일은 서로 일치함" 다르다면 두 파일 전체 출력
-	//str1에 텍스트 내용 저장
-	int str1[1000], str2[1000];
-	int i = 0;
-	while ((str1[i] = fgetc(fp1)) != EOF) { //text가 끝날때까지 반복
-		i++;
-	}
-	str1[i] = '\0'; //문자열의 끝 지정
+	while (1) {
+		ch1 = fgetc(fp1);
+		ch2 = fgetc(fp2);
+		if (ch1 != ch2) { //문자가 다르면
+			same = 0;
+			break;
+		}
 
-	i = 0;
-	while ((str2[i] = fgetc(fp2)) != EOF) { //text가 끝날때까지 반복
-		i++;
-	}
-	str2[i] = '\0'; //문자열의 끝 지정
-
-	printf("<< ");
-	for (i = 0; str1[i] != '\0'; i++) {
-		putchar(str1[i]);
-	}
-	printf(" >> ");
-
-	for (i = 0; str2[i] != '\0'; i++) {
-		putchar(str2[i]);
+		if (ch1 == EOF || ch2 == EOF)
+			break;
 	}
 
+	if (same) printf("파일은 서로 일치함");
+	else {
+		fseek(fp1, 0L, SEEK_SET); //파일 포인터 초기화
+		printf("<< ");
+		while ((ch1 = fgetc(fp1)) != EOF) {
+			printf("%c", ch1);
+		}
+		printf(" >> ");
+
+		fseek(fp2, 0L, SEEK_SET); //파일 포인터 초기화
+		while ((ch2 = fgetc(fp2)) != EOF) {
+			printf("%c", ch2);
+		}
+	}
+
+	//파일 닫기
 	fclose(fp1);
 	fclose(fp2);
+
+	return 0;
 }
