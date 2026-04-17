45d6221ae65e1792a85dcc817e707b836c6c00f4
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-18 03:01:22 +0900

    코드실행결과 추가

--- week07_textcmp/main.c
+++ week07_textcmp/main.c
@@ -1,6 +1,4 @@
 ﻿// 두개의 텍스트 파일을 비교하는 프로그램
-// 파일이 서로 일치함의 조건
-// 1. 두 텍스트 파일의 모든 문자가 같은 문자여야함
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
@@ -24,14 +22,16 @@ int main() {
 	fp2 = fopen(file_name2, "r");
 	if (fp2 == NULL) {
 		printf("두 번째 파일을 찾을 수 없습니다.");
-		fclose(fp1); //
+		fclose(fp1); //fp1 닫기
 		return 1;
 	}
 	
+	//파일 비교문
 	while (1) {
-		ch1 = fgetc(fp1);
+		ch1 = fgetc(fp1); 
 		ch2 = fgetc(fp2);
-		if (ch1 != ch2) { //문자가 다르면
+
+		if (ch1 != ch2) {
 			same = 0;
 			break;
 		}
@@ -40,22 +40,23 @@ int main() {
 			break;
 	}
 
-	if (same) printf("파일은 서로 일치함");
+	//판별 후 처리
+	if (same) 
+		printf("파일은 서로 일치함");
 	else {
-		fseek(fp1, 0L, SEEK_SET); //파일 포인터 초기화
+		fseek(fp1, 0L, SEEK_SET);
 		printf("<< ");
 		while ((ch1 = fgetc(fp1)) != EOF) {
 			printf("%c", ch1);
 		}
 		printf(" >> ");
 
-		fseek(fp2, 0L, SEEK_SET); //파일 포인터 초기화
+		fseek(fp2, 0L, SEEK_SET);
 		while ((ch2 = fgetc(fp2)) != EOF) {
 			printf("%c", ch2);
 		}
 	}
 
-	//파일 닫기
 	fclose(fp1);
 	fclose(fp2);
 
