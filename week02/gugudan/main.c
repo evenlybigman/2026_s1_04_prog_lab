4ab715a29604948e18216ece4c14d7bea661f271
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-20 02:36:19 +0900

    2주차 폴더 리팩토링

---
+++ week02/gugudan/main.c
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int i, l;
+	for (l = 2; l <= 9; l++) {
+		for (i = 1; i <= 9; i++) {
+			printf("%d X %d = %d\n", l, i, l * i);
+		}
+	}
+	return 0;
+}
---
+++ week02/updowngame/main.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	srand(time(NULL));
+	int answer = rand() % 100;
+	int try;
+	int i;
+	int question;
+	try = 0;
+	do {
+		printf("정답을 추측하여 입력하시오: ");
+		scanf("%d", &question);
+		if (question > answer)
+		{
+			printf("down.\n");
+		}
+		else
+		{
+			printf("up.\n");
+		}
+		try++;
+	} while (question != answer);
+	printf("축하합니다. 시도횟수=%d\n", try);
+	return 0;
+}
